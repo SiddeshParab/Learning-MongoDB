668732b8b610a534421d2faf296cbac85320197c
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-10-31 19:21:59 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const path = require("path");
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("6508297a60fbd84122f420c5")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  // console.log(client);
+  app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
+});
